dc07c0a32123a42e6c88b9070e618c54eaa6ac39
Lizsa <blvbev@gmail.com>
2020-11-15 22:44:20 +0300

    Lesson2(before review); add the method update and change others in ArrayStorage

---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,102 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
+
+    //do not forget to change this variable in new methods that change amount of real resumes!
+    private int amountOfResumes = 0;
+
+
+    void clear(){
+
+        Arrays.fill(storage, 0, amountOfResumes - 1, null);
+        amountOfResumes = 0;
+    }
+
+    public boolean contains(String uuid){
+        for (int i = 0; i < size(); i++) {
+            if ((storage[i].getUuid().equals(uuid))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void save(Resume r){
+
+        if (contains(r.getUuid())) {
+            System.out.println("Method save: Резюме(" + r.getUuid() + ") уже добавлено в список!\n");
+        } else {
+            storage[amountOfResumes] = r;
+            amountOfResumes++;
+        }
+
+    }
+
+    public void update(Resume r){
+
+        if (contains(r.getUuid())) {
+            for (int i = 0; i < size(); i++) {
+                if (r == storage[i]) {
+                    storage[i] = r;
+                }
+            }
+        } else {
+            System.out.println("Method update: " + "Резюме(" + r.getUuid() + ") отсутствует в списке!\n");
+        }
+    }
+
+    public Resume get(String uuid){
+
+        if (contains(uuid)) {
+            for (int i = 0; i < amountOfResumes; i++) {
+                if (storage[i].getUuid().equals(uuid)) {
+                    return storage[i];
+                }
+            }
+        }
+        System.out.println("Method get: " + "Резюме(" + uuid + ") отсутствует в списке!\n");
+        return null;
+    }
+
+
+    public void delete(String uuid){
+
+        if (contains(uuid)) {
+            for (int i = 0; i < amountOfResumes; i++) {
+                if (uuid.equals(storage[i].getUuid())) {
+                    System.arraycopy(storage, i + 1, storage, i, amountOfResumes - i);
+                    amountOfResumes--;
+                }
+            }
+        } else {
+            System.out.println("Method delete:  + Резюме(" + uuid + ") отсутствует в списке!\n");
+        }
+
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll(){
+
+        try {
+            return Arrays.copyOfRange(storage, 0, amountOfResumes - 1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Method getAll: Массив пуст!\n");
+        }
+        return null;
+    }
+
+    public int size(){
+        return amountOfResumes;
+    }
+
+}
---
+++ src/com/urise/webapp/storage/MainTestArrayStorage.java
@@ -0,0 +1,57 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+/**
+ * Test for your com.urise.webapp.storage.ArrayStorage implementation
+ */
+public class MainTestArrayStorage {
+    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+
+    public static void main(String[] args){
+        Resume r1 = new Resume();
+        r1.setUuid("uuid1");
+        Resume r2 = new Resume();
+        r2.setUuid("uuid2");
+        Resume r3 = new Resume();
+        r3.setUuid("uuid3");
+
+
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
+
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+
+        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+
+        ARRAY_STORAGE.save(r1);
+        Resume r4 = new Resume();
+        r4.setUuid("uuid4");
+        ARRAY_STORAGE.delete(r4.getUuid());
+        ARRAY_STORAGE.get(r4.getUuid());
+
+        printAll();
+        ARRAY_STORAGE.delete(r1.getUuid());
+        printAll();
+        ARRAY_STORAGE.clear();
+        printAll();
+
+
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+    }
+
+    static void printAll(){
+        System.out.println("\nGet All");
+
+        try {
+            for (Resume r : ARRAY_STORAGE.getAll()) {
+                System.out.println(r);
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Массив пуст, печатать нечего!");
+        }
+
+    }
+}
