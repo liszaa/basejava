d1b6730b18143fe77168beb6fa843d259dd404ae
Lizsa <blvbev@gmail.com>
2020-11-17 17:04:55 +0300

    Lesson 2, changes after second review

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10_000];
 
     //do not forget to change this variable in new methods that change amount of real resumes!
     private int amountOfResumes = 0;
@@ -20,18 +20,11 @@ public class ArrayStorage {
         amountOfResumes = 0;
     }
 
-    public int findResumeIndex(String uuid) {
-        for (int i = 0; i < size(); i++) {
-            if ((storage[i].getUuid().equals(uuid))) {
-                return i;
-            }
-        }
-        return RESUME_NOT_FOUND;
-    }
-
     public void save(Resume resume) {
-        if ((findResumeIndex(resume.getUuid())) != RESUME_NOT_FOUND || amountOfResumes >= storage.length) {
-            System.out.println("Method save: Резюме(" + resume.getUuid() + ") не может быть добавлено в список (резюме уже есть в списке или нет места в storage)\n");
+        if ((findResumeIndex(resume.getUuid())) != RESUME_NOT_FOUND) {
+            System.out.println("Method save: Резюме(" + resume.getUuid() + ") не может быть добавлено в список (резюме уже есть в списке\n");
+        } else if (amountOfResumes >= storage.length) {
+            System.out.println("Method save: Резюме(" + resume.getUuid() + ") не может быть добавлено в список (нет места в storage)\n");
         } else {
             storage[amountOfResumes] = resume;
             amountOfResumes++;
@@ -51,15 +44,14 @@ public class ArrayStorage {
         int index = findResumeIndex(uuid);
         if (index != RESUME_NOT_FOUND) {
             return storage[index];
-        } else {
-            System.out.println("Method get: " + "Резюме(" + uuid + ") отсутствует в списке!\n");
-            return null;
         }
+        System.out.println("Method get: " + "Резюме(" + uuid + ") отсутствует в списке!\n");
+        return null;
     }
 
     public void delete(String uuid) {
-        if (findResumeIndex(uuid) != RESUME_NOT_FOUND) {
-            int index = findResumeIndex(uuid);
+        int index = findResumeIndex(uuid);
+        if (index != RESUME_NOT_FOUND) {
             System.arraycopy(storage, index + 1, storage, index, amountOfResumes - index);
             amountOfResumes--;
         } else {
@@ -73,13 +65,22 @@ public class ArrayStorage {
     public Resume[] getAll() {
         if (amountOfResumes != 0) {
             return Arrays.copyOfRange(storage, 0, amountOfResumes - 1);
-        } else {
-            System.out.println("Method getAll: Массив пуст!\n");
-            return null;
         }
+        System.out.println("Method getAll: Массив пуст!\n");
+        return null;
+
     }
 
     public int size() {
         return amountOfResumes;
+    }
+
+    private int findResumeIndex(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if ((storage[i].getUuid().equals(uuid))) {
+                return i;
+            }
+        }
+        return RESUME_NOT_FOUND;
     }
 }
