6ce46b6934ec0e750574ad686bb426f23223c69f
Lizsa <blvbev@gmail.com>
2020-12-24 09:48:45 +0300

    Finish Tests

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class MyReflection {
+public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume("123");
         System.out.println(r.getClass());
